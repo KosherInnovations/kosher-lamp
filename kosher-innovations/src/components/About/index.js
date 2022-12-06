bba7cae923ce2987f4f430eb9a07127e75a021b1
Moshe <mobgadol@gmail.com>
2022-12-06 12:56:31 -0500

    added a basic about section with the brand name

--- kosher-innovations/src/components/About/index.js
+++ kosher-innovations/src/components/About/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import downArrow from "../../resources/down-arrow.png";
 
 const About = () => {
-    return (
-        <h1>About Page</h1>
-    )
-}
+  return (
+    <section id="about">
+      <div id="about-us">
+        <h1>
+          <span>
+            Kosher<br></br>Innovations
+          </span>
+          <br></br>The KosherLamp People
+        </h1>
+        <h3>Who We Are</h3>
+        <img className="down-arrow" src={downArrow} alt="down arrow"></img>
+      </div>
+    </section>
+  );
+};
 
 export default About;
