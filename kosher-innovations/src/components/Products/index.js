6658b90c18bf70f7a65cdd835e0788a469d1cf9f
Moshe <mobgadol@gmail.com>
2022-12-05 23:29:03 -0500

    created cards for the bug checker and warming tray

--- kosher-innovations/src/components/Products/index.js
+++ kosher-innovations/src/components/Products/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import kLampWhite from "../../resources/white-kmax.png";
+import kLampWhite from "../../resources/kmax/white-kmax.png";
 import travelLampWhite from "../../resources/travel-lamp/white-travel.jpg";
+import bugChecker from "../../resources/bug-checker/bug-checker.jpg";
+import warmingTray from "../../resources/warming-tray/warming-tray.jpg";
 
 const Products = () => {
   return (
@@ -42,6 +44,24 @@ const Products = () => {
               <div className="color-picker" id="travel-brown"></div>
             </div>
             <a href="/">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={bugChecker}></img>
+            <h1>Bug Checker</h1>
+          </div>
+          <div className="content">
+            <a href="https://www.kosherimage.com/cmd.php?pid=e418af322bd347a18412af62a324be4e">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={warmingTray}></img>
+            <h1>Shabbos Safe Warming Tray</h1>
+          </div>
+          <div className="content">
+            <a href="https://www.kosherimage.com/cmd.php?pid=e418af322bd347a18412af62a324be4e">Add To Cart</a>
           </div>
         </div>
       </section>
