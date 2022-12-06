ed9e679d25661349c01c37c390ac171869c8a15f
Moshe <mobgadol@gmail.com>
2022-12-05 23:47:50 -0500

    created cards for the scrub gloves, smart shissel, go wash and kosherClock

--- kosher-innovations/src/components/Products/index.js
+++ kosher-innovations/src/components/Products/index.js
@@ -3,6 +3,10 @@ import kLampWhite from "../../resources/kmax/white-kmax.png";
 import travelLampWhite from "../../resources/travel-lamp/white-travel.jpg";
 import bugChecker from "../../resources/bug-checker/bug-checker.jpg";
 import warmingTray from "../../resources/warming-tray/warming-tray.jpg";
+import scrubGlovesRed from "../../resources/scrub-gloves/gloves-red.png";
+import smartShissel from "../../resources/smart-shissel/smart-shissel.jpg";
+import goWash from "../../resources/go-wash/go-wash.jpg";
+import kosherClock from "../../resources/kosher-clock/kosher-clock.jpg";
 
 const Products = () => {
   return (
@@ -48,7 +52,7 @@ const Products = () => {
         </div>
         <div className="card">
           <div className="imgBox">
-            <img src={bugChecker}></img>
+            <img src={bugChecker} alt="bug checker"></img>
             <h1>Bug Checker</h1>
           </div>
           <div className="content">
@@ -57,11 +61,52 @@ const Products = () => {
         </div>
         <div className="card">
           <div className="imgBox">
-            <img src={warmingTray}></img>
+            <img src={warmingTray} alt="shabbos warming tray"></img>
             <h1>Shabbos Safe Warming Tray</h1>
           </div>
           <div className="content">
             <a href="https://www.kosherimage.com/cmd.php?pid=e418af322bd347a18412af62a324be4e">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={scrubGlovesRed} alt="shabbos scrub gloves"></img>
+            <h1>Shabbos Scrub Gloves</h1>
+          </div>
+          <div className="content">
+            <h3>Color :</h3>
+            <div className="color">
+              <div className="color-picker" id="gloves-red"></div>
+              <div className="color-picker" id="gloves-blue"></div>
+            </div>
+            <a href="/">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={smartShissel} alt="smart shissel"></img>
+            <h1>Smart Shissel</h1>
+          </div>
+          <div className="content">
+            <a href="/">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={goWash} alt="go wash"></img>
+            <h1>Go Wash</h1>
+          </div>
+          <div className="content">
+            <a href="/">Add To Cart</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgBox">
+            <img src={kosherClock} alt="kosher clock"></img>
+            <h1>Kosher Clock</h1>
+          </div>
+          <div className="content">
+            <a href="/">Add To Cart</a>
           </div>
         </div>
       </section>
