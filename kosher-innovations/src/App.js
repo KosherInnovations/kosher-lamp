0eac75ed3b57ffe11c49b612cd3c2f042d9c1d6f
Moshe KosherLamp <kosherlamp@gmail.com>
2023-02-09 16:41:56 -0500

    changed bg image for all sections

--- kosher-innovations/src/App.js
+++ kosher-innovations/src/App.js
@@ -145,6 +145,18 @@ function App() {
           currentPage={currentPage}
         ></Navbar>
       </Header>
+      <div
+        className="overlay"
+        style={{
+          position: "fixed",
+          top: "0",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0,0,0,0.5)",
+          zIndex: 0,
+        }}
+      ></div>
       <main>
         <Page currentPage={currentPage}></Page>
       </main>
