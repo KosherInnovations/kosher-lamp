2f901ba4f3f755832c94ec754e3dc6628616300c
Moshe KosherLamp <kosherlamp@gmail.com>
2023-02-10 12:11:50 -0500

    created a grid for 2 cards, added a banner

--- kosher-innovations/src/components/HomePage/index.js
+++ kosher-innovations/src/components/HomePage/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import homeBanner from "../../resources/ki-logo.webp";
 
 const HomePage = () => {
-    return (
-        <h1>Hello World</h1>
-    )
-}
+  return (
+    <section id="home">
+      <div className="banner-container">
+        <img className="banner" src={homeBanner} alt="Kosher Innovations homepage banner"></img>
+      </div>
+      <div className="display-grid">
+        <div className="display-card card-1"></div>
+        <div className="display-card card-2"></div>
+      </div>
+    </section>
+  );
+};
 
 export default HomePage;
